579fb5e396703b6374bd58c91bc14e07689a0b72
jaanisfehling <jaanisfehling@gmail.com>
2023-05-22 19:00:50 +0200

    sheet 2 done in functional style

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -4,7 +4,7 @@ case class Sum(left: Expr, right: Expr) extends Expr
 case class Product(left: Expr, right: Expr) extends Expr
 case class Var(name: String) extends Expr
 
-def eval(expr: Expr, vars: Map[String, Expr]): Int = expr match {
+def eval(expr: Expr, vars: Map[String, Expr]): Int = expr match
   case Num(value) =>
     value
   case Var(name) =>
@@ -13,10 +13,9 @@ def eval(expr: Expr, vars: Map[String, Expr]): Int = expr match {
     eval(left, vars) + eval(right, vars)
   case Product(left, right) =>
     eval(left, vars) * eval(right, vars)
-}
 
-def main(args: Array[String]): Unit = {
+
+def main(args: Array[String]): Unit =
   val expr = Product(Var("x"), Product(Num(1), Num(3)))
   val vars = Map("x" -> Sum(Num(2), Num(3)))
   println(eval(expr, vars))
-}
--- src/main/scala/Niki.scala
+++ src/main/scala/Niki.scala
@@ -1,22 +1,53 @@
+import scala.annotation.tailrec
+
 trait Command
-class Left extends Command
-class Right extends Command
-class Forward(number: Int) extends Command
+case class Left() extends Command
+case class Right() extends Command
+case class Forward(number: Int) extends Command
 
 enum Direction:
   case North, East, South, West
 
-class State(position: (Int, Int), direction: Direction)
+case class State(position: (Int, Int), direction: Direction)
 
-//def execute(cmds: Seq[Command], state: State): State = cmds.head match {
-//  case Left() => state match {
-//    case State(position, North) =>
-//      execute(cmds.drop(1), State(position, West))
-//    case State(position, East) =>
-//      execute(cmds.drop(1), State(position, North))
-//    case State(position, South) =>
-//      execute(cmds.drop(1), State(position, East))
-//    case State(position, West) =>
-//      execute(cmds.drop(1), State(position, South))
-//  }
-//}
+@tailrec
+def execute(cmds: Seq[Command], state: State): State =
+  if cmds.isEmpty then state
+  else cmds.head match {
+    case Left() => state match {
+      case State(position, Direction.North) =>
+        execute(cmds.drop(1), State(position, Direction.West))
+      case State(position, Direction.East) =>
+        execute(cmds.drop(1), State(position, Direction.North))
+      case State(position, Direction.South) =>
+        execute(cmds.drop(1), State(position, Direction.East))
+      case State(position, Direction.West) =>
+        execute(cmds.drop(1), State(position, Direction.South))
+    }
+    case Right() => state match {
+      case State(position, Direction.North) =>
+        execute(cmds.drop(1), State(position, Direction.East))
+      case State(position, Direction.East) =>
+        execute(cmds.drop(1), State(position, Direction.South))
+      case State(position, Direction.South) =>
+        execute(cmds.drop(1), State(position, Direction.West))
+      case State(position, Direction.West) =>
+        execute(cmds.drop(1), State(position, Direction.North))
+    }
+    case Forward(n) => state match {
+      case State(position, Direction.North) =>
+        execute(cmds.drop(1), State((position(0), position(1)+n), Direction.North))
+      case State(position, Direction.East) =>
+        execute(cmds.drop(1), State((position(0)+n, position(1)), Direction.East))
+      case State(position, Direction.South) =>
+        execute(cmds.drop(1), State((position(0), position(1)-n), Direction.South))
+      case State(position, Direction.West) =>
+        execute(cmds.drop(1), State((position(0)-n, position(1)), Direction.West))
+    }
+}
+
+@main def main() =
+  val state = State((0, 0), Direction.North)
+  val program = List(Forward(1), Left(), Forward(2))
+  val finalState = execute(program, state)
+  println(finalState.position(0))
