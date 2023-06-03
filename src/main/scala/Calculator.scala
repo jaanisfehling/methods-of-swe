18c3428075ef36bc830e48b086bb17c53d9889f1
jaanisfehling <jaanisfehling@gmail.com>
2023-06-03 23:00:40 +0200

    try sheet 3 with pattern matching only

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -1,5 +1,7 @@
+import scala.reflect.ClassTag
+
 trait Expression
-case class Constant(value: AnyVal) extends Expression
+case class Constant(value: Int | Boolean) extends Expression
 case class Variable(name: String) extends Expression
 
 trait Operator extends Expression
@@ -12,9 +14,13 @@ trait Command
 case class Assign(variable: Variable, expression: Expression) extends Command
 case class Print(expression: Expression) extends Command
 
+def isTypeCorrect(a: Int | Boolean, b: Int | Boolean): Boolean =
+  val B = ClassTag(b.getClass)
+  ClassTag(a.getClass) match
+    case B => true
+    case _ => false
 
-
-def eval(expr: Expression, vars: Map[String, Expression]): Int = expr match
+def eval(expr: Expression, vars: Map[String, Expression]): Int | Boolean = expr match
   case Constant(value) =>
     value
   case Variable(name) =>
@@ -23,9 +29,12 @@ def eval(expr: Expression, vars: Map[String, Expression]): Int = expr match
     eval(left, vars) + eval(right, vars)
   case Product(left, right) =>
     eval(left, vars) * eval(right, vars)
-
+  case And(left, right) =>
+    eval(left, vars) & eval(right, vars)
+  case Or(left, right) =>
+    eval(left, vars) | eval(right, vars)
 
 @main def main(): Unit =
   val expr = Product(Variable("x"), Product(Constant(1), Constant(3)))
-  val vars = Map("x" -> Sum(Constant(2), Constant(3)))
+  val vars = Map("x" -> Sum(Constant(2), Constant(3)), "isTrue" -> Or(Constant(true), Constant(false)))
   println(eval(expr, vars))
